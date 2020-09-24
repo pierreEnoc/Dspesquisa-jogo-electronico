152bed0a6a457e29bedc95c46748c49c7c1cb1a6
pierreEnoc <pierre_enoc@yahoo.fr>
2020-09-24 11:20:47 -0300

    :sparkls: start project

---
+++ Backend-DsPesquisa/Pesquisar de jogo/Pesquisar-de-jogo/src/main/java/despesquisa/Pesquisar/de/jogo/PesquisarDeJogoApplication.java
@@ -0,0 +1,13 @@
+package despesquisa.Pesquisar.de.jogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisarDeJogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesquisarDeJogoApplication.class, args);
+	}
+
+}
---
+++ Backend-DsPesquisa/Pesquisar de jogo/Pesquisar-de-jogo/src/test/java/despesquisa/Pesquisar/de/jogo/PesquisarDeJogoApplicationTests.java
@@ -0,0 +1,13 @@
+package despesquisa.Pesquisar.de.jogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisarDeJogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
